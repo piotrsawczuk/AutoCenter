0925c3f4b99ca624fcb33cb33cb7aad5b1ef93ba
piotrsawczuk <piotr.sawczuk0@gmail.com>
2018-01-30 14:19:55 +0100

    Initial commit

---
+++ src/test/java/sawczuk/AutoCenter/AutoCenterApplicationTests.java
@@ -0,0 +1,16 @@
+package sawczuk.AutoCenter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutoCenterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
