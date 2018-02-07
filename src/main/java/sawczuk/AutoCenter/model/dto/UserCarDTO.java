15c48177f145ec22cf85d2eae2b743f2bd2aa4d9
piotrsawczuk <piotr.sawczuk0@gmail.com>
2018-02-07 23:52:33 +0100

    Added UserDetailDTO. Removed field username from UserCarDTO.

--- src/main/java/sawczuk/AutoCenter/model/dto/UserCarDTO.java
+++ src/main/java/sawczuk/AutoCenter/model/dto/UserCarDTO.java
@@ -1,13 +1,7 @@
 package sawczuk.AutoCenter.model.dto;
 
 public class UserCarDTO {
-    //temp, later logged in username
-    private String username;
     private Long carApiId;
-
-    public String getUsername() {
-        return username;
-    }
 
     public Long getCarApiId() {
         return carApiId;
---
+++ src/main/java/sawczuk/AutoCenter/model/dto/UserDetailDTO.java
@@ -0,0 +1,34 @@
+package sawczuk.AutoCenter.model.dto;
+
+public class UserDetailDTO {
+    private String firstname;
+    private String surname;
+    private String address;
+    private String city;
+    private String zipCode;
+    private String phoneNumber;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
