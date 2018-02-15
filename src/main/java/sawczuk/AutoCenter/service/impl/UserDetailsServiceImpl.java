2ed17ecfaf94daba5c95e4fe9cf41012158683e1
piotrsawczuk <piotr.sawczuk0@gmail.com>
2018-02-16 00:04:56 +0100

    Refactoring

--- src/main/java/sawczuk/AutoCenter/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/sawczuk/AutoCenter/service/impl/UserDetailsServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import sawczuk.AutoCenter.model.Role;
 import sawczuk.AutoCenter.model.User;
 import sawczuk.AutoCenter.repository.UserRepository;
@@ -26,9 +25,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
 
     @Override
-    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsernameIgnoreCase(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("The username %username doesn't exist", username));
+        }
 
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
         for (Role role : user.getRoles()) {
