4de38c16bd49d417d7f538aa8ee25ae85d9745cf
piotrsawczuk <piotr.sawczuk0@gmail.com>
2018-02-20 19:04:37 +0100

    Added TransactionSystemException handler to catch exceptions thrown while fields are not valid when inserting/updating object in database. Moved ExceptionHandlers to top of class.

--- src/main/java/sawczuk/AutoCenter/exception/RestExceptionHandler.java
+++ src/main/java/sawczuk/AutoCenter/exception/RestExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.transaction.TransactionSystemException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -71,6 +72,91 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     /**
+     * Handles javax.validation.ConstraintViolationException. Thrown when @Validated fails.
+     *
+     * @param ex the ConstraintViolationException
+     * @return the ApiError object
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    protected ResponseEntity<Object> handleConstraintViolation(
+            ConstraintViolationException ex,
+            WebRequest request) {
+        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
+        String path = servletWebRequest.getRequest().getServletPath();
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex, path);
+        apiError.setMessage("Validation error");
+        apiError.addValidationErrors(ex.getConstraintViolations());
+        return buildResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(TransactionSystemException.class)
+    protected ResponseEntity<Object> handleTransactionSystemException(
+            TransactionSystemException ex,
+            WebRequest request) {
+        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
+        String path = servletWebRequest.getRequest().getServletPath();
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getRootCause().getMessage(), ex, path);
+        if (ex.getRootCause() instanceof ConstraintViolationException)
+            apiError.addValidationErrors(((ConstraintViolationException) ex.getRootCause()).getConstraintViolations());
+        return buildResponseEntity(apiError);
+    }
+
+    /**
+     * Handles EntityNotFoundException. Created to encapsulate errors with more detail than javax.persistence.EntityNotFoundException.
+     *
+     * @param ex the EntityNotFoundException
+     * @return the ApiError object
+     */
+    @ExceptionHandler(EntityNotFoundException.class)
+    protected ResponseEntity<Object> handleEntityNotFound(
+            EntityNotFoundException ex,
+            WebRequest request) {
+        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
+        String path = servletWebRequest.getRequest().getServletPath();
+        return buildResponseEntity(new ApiError(HttpStatus.NOT_FOUND, ex.getMessage(), ex, path));
+    }
+
+    /**
+     * Handle DataIntegrityViolationException, inspects the cause for different DB causes.
+     *
+     * @param ex the DataIntegrityViolationException
+     * @return the ApiError object
+     */
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    protected ResponseEntity<Object> handleDataIntegrityViolation(
+            DataIntegrityViolationException ex,
+            WebRequest request) {
+        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
+        String path = servletWebRequest.getRequest().getServletPath();
+        if (ex.getCause() instanceof ConstraintViolationException) {
+            return buildResponseEntity(new ApiError(HttpStatus.CONFLICT, "Database error", ex.getCause(), path));
+        }
+        return buildResponseEntity(new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex, path));
+    }
+
+    /**
+     * Handle Exception, handle generic Exception.class
+     *
+     * @param ex the Exception
+     * @return the ApiError object
+     */
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<Object> handleMethodArgumentTypeMismatch(
+            MethodArgumentTypeMismatchException ex,
+            WebRequest request) {
+        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
+        String path = servletWebRequest.getRequest().getServletPath();
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex, path);
+        apiError.setMessage(String.format(
+                "The parameter '%s' of value '%s' could not be converted to type '%s'",
+                ex.getName(),
+                ex.getValue(),
+                ex.getRequiredType().getSimpleName()));
+        apiError.setDebugMessage(ex.getMessage());
+        return buildResponseEntity(apiError);
+    }
+
+    /**
      * Handle MissingServletRequestParameterException. Triggered when a 'required' request parameter is missing.
      *
      * @param ex      MissingServletRequestParameterException
@@ -90,7 +176,6 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         String error = ex.getParameterName() + " parameter is missing";
         return buildResponseEntity(new ApiError(HttpStatus.BAD_REQUEST, error, ex, path));
     }
-
 
     /**
      * Handle HttpMediaTypeNotSupportedException. This one triggers when JSON is invalid as well.
@@ -145,39 +230,6 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     /**
-     * Handles javax.validation.ConstraintViolationException. Thrown when @Validated fails.
-     *
-     * @param ex the ConstraintViolationException
-     * @return the ApiError object
-     */
-    @ExceptionHandler(javax.validation.ConstraintViolationException.class)
-    protected ResponseEntity<Object> handleConstraintViolation(
-            javax.validation.ConstraintViolationException ex,
-            WebRequest request) {
-        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
-        String path = servletWebRequest.getRequest().getServletPath();
-        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex, path);
-        apiError.setMessage("Validation error");
-        apiError.addValidationErrors(ex.getConstraintViolations());
-        return buildResponseEntity(apiError);
-    }
-
-    /**
-     * Handles EntityNotFoundException. Created to encapsulate errors with more detail than javax.persistence.EntityNotFoundException.
-     *
-     * @param ex the EntityNotFoundException
-     * @return the ApiError object
-     */
-    @ExceptionHandler(EntityNotFoundException.class)
-    protected ResponseEntity<Object> handleEntityNotFound(
-            EntityNotFoundException ex,
-            WebRequest request) {
-        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
-        String path = servletWebRequest.getRequest().getServletPath();
-        return buildResponseEntity(new ApiError(HttpStatus.NOT_FOUND, ex.getMessage(), ex, path));
-    }
-
-    /**
      * Handle HttpMessageNotReadableException. Happens when request JSON is malformed.
      *
      * @param ex      HttpMessageNotReadableException
@@ -219,48 +271,6 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         String error = "Error writing JSON output";
         return buildResponseEntity(new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, error, ex, path));
     }
-
-
-    /**
-     * Handle DataIntegrityViolationException, inspects the cause for different DB causes.
-     *
-     * @param ex the DataIntegrityViolationException
-     * @return the ApiError object
-     */
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    protected ResponseEntity<Object> handleDataIntegrityViolation(
-            DataIntegrityViolationException ex,
-            WebRequest request) {
-        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
-        String path = servletWebRequest.getRequest().getServletPath();
-        if (ex.getCause() instanceof ConstraintViolationException) {
-            return buildResponseEntity(new ApiError(HttpStatus.CONFLICT, "Database error", ex.getCause(), path));
-        }
-        return buildResponseEntity(new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex, path));
-    }
-
-    /**
-     * Handle Exception, handle generic Exception.class
-     *
-     * @param ex the Exception
-     * @return the ApiError object
-     */
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    protected ResponseEntity<Object> handleMethodArgumentTypeMismatch(
-            MethodArgumentTypeMismatchException ex,
-            WebRequest request) {
-        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
-        String path = servletWebRequest.getRequest().getServletPath();
-        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex, path);
-        apiError.setMessage(String.format(
-                "The parameter '%s' of value '%s' could not be converted to type '%s'",
-                ex.getName(),
-                ex.getValue(),
-                ex.getRequiredType().getSimpleName()));
-        apiError.setDebugMessage(ex.getMessage());
-        return buildResponseEntity(apiError);
-    }
-
 
     private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
         return new ResponseEntity<>(apiError, apiError.getStatus());
