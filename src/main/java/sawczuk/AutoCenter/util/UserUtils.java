1183b8a3279ee13e41dd404c7f6b384708f5ecdc
piotrsawczuk <piotr.sawczuk0@gmail.com>
2018-03-23 12:19:18 +0100

    Added method hasLoggedInRoleAdmin.

--- src/main/java/sawczuk/AutoCenter/util/UserUtils.java
+++ src/main/java/sawczuk/AutoCenter/util/UserUtils.java
@@ -1,5 +1,6 @@
 package sawczuk.AutoCenter.util;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -14,6 +15,19 @@ public class UserUtils {
             return ((UserDetails) principal).getUsername();
         } else {
             return principal.toString();
+        }
+    }
+
+    public static boolean hasLoggedInRoleAdmin() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            for (GrantedAuthority role : ((UserDetails) principal).getAuthorities()) {
+                if (role.getAuthority().equals("admin"))
+                    return true;
+            }
+            return false;
+        } else {
+            return false;
         }
     }
 }
