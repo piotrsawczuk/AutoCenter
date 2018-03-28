184ff1decf08067b036f39bf067e706e9f32bd96
Piotr Sawczuk <piotr.sawczuk0@gmail.com>
2018-03-28 11:48:52 +0200

    Different formatting.

--- src/index.js
+++ src/index.js
@@ -9,5 +9,9 @@ import { Provider } from 'react-redux';
 import rootReducer from './reducers/rootReducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
+
 ReactDOM.render(<Provider store = {store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
