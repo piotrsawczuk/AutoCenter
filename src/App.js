31b2def1cb7d8c49d47fe3e8335c1be7a7356df9
Piotr Sawczuk <piotr.sawczuk0@gmail.com>
2018-03-28 12:00:44 +0200

    Added LoginPage to routing and added missing imports.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import MainPage from './containers/MainPage';
+import Navbar from './components/Navbar';
+import LoginPage from './containers/LoginPage';
 
 class App extends Component {
   render() {
@@ -8,6 +12,7 @@ class App extends Component {
         <div className = 'ui container'>
           <Switch>
             <Route exact path = '/' component = {MainPage}/>
+            <Route exact path = '/login' component = {LoginPage}/>
           </Switch>
         </div>
       </div>
