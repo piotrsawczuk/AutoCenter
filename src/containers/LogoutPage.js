be51a622a2fd1deb9e520e7a0e1503ae4a2f0aa3
Piotr Sawczuk <piotr.sawczuk0@gmail.com>
2018-03-28 17:02:54 +0200

    Added LogoutPage to delete token from localStorage.

---
+++ src/containers/LogoutPage.js
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+import PropTypes from "prop-types";
+import { connect } from 'react-redux';
+import { logout } from '../actions/authentication';
+
+class LogoutPage extends Component {
+    static propTypes = {
+        dispatch: PropTypes.func.isRequired
+    };
+
+    componentWillMount() {
+        this.props.dispatch(logout());
+        this.props.history.push('/');
+    }
+
+    render() {
+        return null;
+    }
+}
+
+export default connect()(LogoutPage);
