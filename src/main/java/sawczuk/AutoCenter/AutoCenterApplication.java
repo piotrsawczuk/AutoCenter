9b30105b0b1977e05f981009249fd0742ddf5aae
Piotr Sawczuk <piotr.sawczuk0@gmail.com>
2018-04-23 02:47:49 +0200

    Modified application for war deployment.

--- src/main/java/sawczuk/AutoCenter/AutoCenterApplication.java
+++ src/main/java/sawczuk/AutoCenter/AutoCenterApplication.java
@@ -2,9 +2,16 @@ package sawczuk.AutoCenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AutoCenterApplication {
+public class AutoCenterApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AutoCenterApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AutoCenterApplication.class, args);
