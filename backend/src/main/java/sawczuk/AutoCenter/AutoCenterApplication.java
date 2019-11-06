5bd07d9db55695b301d863e89909a90fd54f0804
Piotr Sawczuk <piotr.sawczuk0@gmail.com>
2019-11-06 17:02:26 +0100

    Added docker-compose and liquibase to application to be able to run anywhere. Updated README file and added TODO list file.

--- backend/src/main/java/sawczuk/AutoCenter/AutoCenterApplication.java
+++ backend/src/main/java/sawczuk/AutoCenter/AutoCenterApplication.java
@@ -2,16 +2,9 @@ package sawczuk.AutoCenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AutoCenterApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(AutoCenterApplication.class);
-    }
+public class AutoCenterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AutoCenterApplication.class, args);
--- backend/src/main/java/sawczuk/AutoCenter/model/Repair.java
+++ backend/src/main/java/sawczuk/AutoCenter/model/Repair.java
@@ -29,7 +29,7 @@ public class Repair {
     @Column(name = "description")
     private String description;
     @DecimalMin(value = "0.0", message = "The value must be positive")
-    @Column(name = "cost")
+    @Column(name = "cost", columnDefinition = "NUMERIC(8,2)")
     private Double cost;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "exploitation_type")
